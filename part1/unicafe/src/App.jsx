6fff693568af7bf27169d5d46b2ef26fa97a2ba5
Viper292 <crazymelon@hotmail.es>
2024-05-23 19:52:37 -0300

    Solves exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,6 +5,12 @@ const Button = (props) => (
 )
 
 const Display = (props) => {
+  if (props.text === "positive") {
+    return (
+      <div>{props.text} {props.state} %</div>
+    )
+  }
+
   return (
     <div>{props.text} {props.state}</div>
   )
@@ -27,6 +33,9 @@ const Statistics = (props) => {
       <Display text="good" state={props.good} />
       <Display text="neutral" state={props.neutral} />
       <Display text="bad" state={props.bad} />
+      <Display text="all" state={props.all} />
+      <Display text="average" state={props.average} />
+      <Display text="positive" state={props.positive} />
     </div>
   )
 }
@@ -35,27 +44,47 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
 
   const handleButtonClick = (which) => () => {
+    let updatedGood = good
+    let updatedBad = bad
+
     switch(which) {
       case "good":
-        setGood(good + 1);
-        break;
+        setGood(good + 1)
+        updatedGood = good + 1
+        break
       case "neutral":
-        setNeutral(neutral + 1);
-        break;
+        setNeutral(neutral + 1)
+        break
       case "bad":
-        setBad(bad + 1);
-        break;
+        setBad(bad + 1)
+        updatedBad = bad + 1
+        break
       default:
         console.log("This log should never appear! Expression:", which);
     }
+    
+    setAll(all + 1)
+    const difference = updatedGood - updatedBad
+    setAverage(difference / (all + 1))
+    setPositive((updatedGood / (all + 1)) * 100)
   }
 
   return (
     <div>
       <Feedback handleClick={handleButtonClick} />
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      <Statistics 
+        good={good} 
+        neutral={neutral} 
+        bad={bad} 
+        all={all}
+        average={average}
+        positive={positive}
+      />
     </div>
   )
 }
